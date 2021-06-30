7729fb7c892001fbc7cebba6414b86fcf40f761f
abdu-zeyad <abd.d@live.com>
2021-06-30 15:54:33 +0300

    adding components

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./Main.css";
+export class Main extends Component {
+  render() {
+    return (
+      <div className="main-root">
+        <h1>main</h1>
+      </div>
+    );
+  }
+}
+
+export default Main;
